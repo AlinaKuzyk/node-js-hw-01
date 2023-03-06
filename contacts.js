570853f041fe894e60e86e5f24f1ffc7664e300a
Alina_Kuzyk <dr.alinakuzyk@gmail.com>
2023-03-06 12:53:23 +0100

    added all tasks

--- contacts.js
+++ contacts.js
@@ -1,39 +1,48 @@
 const fs = require("fs").promises;
 const path = require("path");
+const uniqid = require("uniqid");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 async function listContacts() {
   try {
-    const contacts = await fs.readFile(contactsPath, "utf-8");
-    return contacts;
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
   } catch (error) {
     console.log(error);
   }
 }
-listContacts();
-// async function getContactById(contactId) {
-//   const contacts = await listContacts();
-//   const contact = contacts.find((contact) => contact.id === contactId);
-//   return contact;
-// }
-// getContactById();
 
-// async function removeContact(contactId) {
-//   const contacts = await listContacts();
-//   const contactToRemove = contacts.filter(
-//     (contact) => contact.id !== contactId
-//   );
-//   await fs.writeFile(contactsPath, JSON.stringify(contactToRemove));
-//   return contactToRemove;
-// }
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  if (!contact) {
+    return null;
+  }
+  return contact;
+}
 
-// async function addContact(name, email, phone) {
-//   const contacts = await listContacts();
-//   const newContact = { name, email, phone };
-//   contacts.push(newContact);
-//   await fs.writeFile(contactsPath, JSON.stringify(newContact));
-//   return newContact;
-// }
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const contactToRemove = contacts.filter(
+    (contact) => contact.id !== contactId
+  );
+  if (!contactToRemove) {
+    return null;
+  }
+  await fs.writeFile(contactsPath, JSON.stringify(contactToRemove));
+  return contactToRemove;
+}
 
-// module.exports = { listContacts, getContactById, removeContact, addContact };
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+   const newContact = { name, email, phone, id: uniqid() };
+    if (!newContact) {
+      return null;
+    }
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
+
+module.exports = { listContacts, getContactById, addContact, removeContact };
--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-// const contacts = require("./contacts.js");
+const { Command } = require("commander");
+const contacts = require("./contacts.js");
 
-// contacts.listContacts();
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contactsList = await contacts.listContacts();
+      console.table(contactsList);
+      break;
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      if (!newContact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      if (!removedContact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
